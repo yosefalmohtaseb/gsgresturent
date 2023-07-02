6cbb7709de924910245b38e0787bf18bceafcc91
yosefalmohtaseb <yaofooo@gmail.com>
2023-07-02 18:01:39 +0300

    the most of hero section

--- src/sections/heroSection/HeroSection.jsx
+++ src/sections/heroSection/HeroSection.jsx
@@ -6,6 +6,11 @@ function HeroSection() {
   return (
     <div className='background-section'>
       <Navbar />
+      <div className='welcom-msg'>
+        <h1>Welcome To Golden View Dine</h1>
+        <p>Explore the authentic vegan dishes with your frinds and family</p>
+        <button>Read more</button>
+      </div>
     </div>
   )
 }
