7db6c8259421aa1c6873d6e06068116128023001
yosefalmohtaseb <yaofooo@gmail.com>
2023-07-02 15:43:34 +0300

    built the main structer and some inetial elaments

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './index.css';
+import HeroSection from './sections/heroSection/HeroSection';
+
+function App() {
+  return (
+    <div>
+      <HeroSection/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/sections/heroSection/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+import Navbar from '../../components/navbar/Navbar'
+
+function HeroSection() {
+  return (
+    <div className='background-container'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default HeroSection
